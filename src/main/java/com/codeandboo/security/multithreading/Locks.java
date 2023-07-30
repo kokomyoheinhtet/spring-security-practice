378092c8b706c555c8cd1be401b9891b2c6faa65
kokomyoheinhtet <kokomyoheinhtet@gmail.com>
2023-07-30 20:07:02 +0700

    Added Locks using ReentrantLock()

---
+++ src/main/java/com/codeandboo/security/multithreading/Locks.java
@@ -0,0 +1,40 @@
+package com.codeandboo.security.multithreading;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Locks {
+    private static int counter = 0;
+    private static Lock lock = new ReentrantLock();
+
+    private static void increment() {
+
+        lock.lock();
+        for (int i = 0; i < 10000; i++) {
+            counter++;
+        }
+        lock.unlock();
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            increment();
+        });
+
+        Thread t2 = new Thread(() -> {
+            increment();
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Counter is " + counter);
+    }
+}
