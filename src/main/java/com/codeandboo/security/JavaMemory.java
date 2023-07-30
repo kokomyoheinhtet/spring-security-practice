c41f9551379ee41f3241a06583ff1aebae89d476
kokomyoheinhtet <kokomyoheinhtet@gmail.com>
2023-07-30 17:15:26 +0700

    added synchronized increment method

---
+++ src/main/java/com/codeandboo/security/JavaMemory.java
@@ -0,0 +1,39 @@
+package com.codeandboo.security;
+
+public class JavaMemory {
+    public static int counter = 0;
+
+    public static synchronized void increment() {
+        counter++;
+    }
+
+    public static void process() {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                increment();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                increment();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Counter is: " + counter);
+    }
+
+    public static void main(String[] args) {
+        process();
+    }
+}
